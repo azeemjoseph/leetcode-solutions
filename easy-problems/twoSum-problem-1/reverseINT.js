580516b550cb5cc84299e27b4da958a225be660a
Azeem Joseph <Azeem.joseph@gmail.com>
2021-09-04 05:36:58 +0500

    ReverseInt second option added

--- easy-problems/twoSum-problem-1/reverseINT.js
+++ easy-problems/twoSum-problem-1/reverseINT.js
@@ -14,3 +14,13 @@ var reverse = function(x) {
         return Reverse_No;
     }
 };
+
+/* var reverseN = 0;
+var sign = n < 0;
+n = Math.abs(n);
+while (n) {
+    reverseN = reverseN*10 + (n % 10);
+    n = Math.floor(n/10);
+}
+return reverseN > 0x7FFFFFFF ? 0 : sign ? -reverseN : reverseN;
+*/
