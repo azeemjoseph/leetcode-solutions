47c1015d701ad71e4388483a26fda7dfb2f69db6
Azeem Joseph <Azeem.joseph@gmail.com>
2021-09-07 02:39:38 +0500

    roman to numbers

---
+++ easy-problems/twoSum-problem-1/roman_numeral.js
@@ -0,0 +1,20 @@
+var romanToInt = function (s) {
+  const Symbols_values = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  let ans = 0;
+  var len = s.length;
+  for (let i = len - 1; ~i; i--) {
+    let total_number = Symbols_values[s.charAt(i)];
+    if (2 * total_number < ans) ans -= total_number;    
+    else ans += total_number;
+  }
+  return ans;
+};
+  
+console.log(romanToInt("LVIII"));
+
+/* Alternate
+const romanToNumber = s => Uint16Array.from(s, n => Symbols_values[n])
+    .reverse()
+    .reduce((sum,value) => sum + (value * 4 < sum ? -value : value)
+    );
+*/
